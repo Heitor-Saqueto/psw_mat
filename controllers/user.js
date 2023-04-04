dda5f54ec29adfdcccb47104ee7685da34150192
Fausto G. Cintra <professor@faustocintra.com.br>
2023-04-04 11:05:32 -0300

    (04/04) Início da implementação da segurança de API

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 // Importar o model correspondente ao controller
 const { User } = require('../models')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const controller = {}   // Objeto vazio
 
@@ -14,6 +16,10 @@ const controller = {}   // Objeto vazio
 
 controller.create = async (req, res) => {
   try {
+
+    // Criptografa a senha
+    req.body.password = await bcrypt.hash(req.body.password, 12)
+
     await User.create(req.body)
     // HTTP 201: Created
     res.status(201).end()
@@ -52,6 +58,13 @@ controller.retrieveOne = async (req, res) => {
 
 controller.update = async (req, res) => {
   try {
+
+    // Se houver sido passado o campo "password",
+    // criptografa a senha
+    if(req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 12)
+    }
+
     const response = await User.update(
       req.body,
       { where: { id: req.params.id }}
@@ -87,6 +100,42 @@ controller.delete = async (req, res) => {
     else {          // Não encontrou e não excluiu
       // HTTP 404: Not found
       res.status(404).end()
+    }
+  }
+  catch(error) {
+    console.error(error)
+  }
+}
+
+controller.login = async (req, res) => {
+  try {
+    const user = await User.scope('withPassword').findOne({ where: { email: req.body.email } })
+
+    // Usuário não encontrado ~> HTTP 401: Unauthorized
+    if(!user) return res.status(401).end()
+
+    const pwMatches = await bcrypt.compare(req.body.password, user.password)
+
+    if(pwMatches) {
+      // A senha confere
+      const token = jwt.sign({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          verified_email: user.verified_email,
+          is_admin: user.is_admin,
+          phone: user.phone
+        },
+        process.env.TOKEN_SECRET,    // Chave para criptografar o token
+        { expiresIn: '24h' }         // Duração do token
+      )
+
+      // Retorna o token ~> HTTP 200: OK (implícito)
+      res.json({ auth: true, token })
+    }
+    else {
+      // Senha errada ~> HTTP 401: Unauthorized
+      res.status(401).end()
     }
   }
   catch(error) {
